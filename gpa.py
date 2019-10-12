81657898bb80d9af541af1ee73b589ede4e27061
wadaka0821 <wadaka0821@gmail.com>
2019-10-12 18:16:00 +0900

    finished main_menu_layout

--- gpa.py
+++ gpa.py
@@ -1,14 +1,7 @@
 import re
 
-def check_filename(name):
-    find = re.search(r".+\.csv",name)
-    if find:
-        return True
-    else:
-        return False
-
 filename = list()
-options = ["ファイルの追加","ファイルの削除","ファイル操作","終了"]
+options = {1:"ファイルの追加",2:"ファイルの削除",3:"ファイル操作",4:"終了"}
 while True:
     print("-----------working space-----------")
     if filename == list():
@@ -16,6 +9,17 @@ while True:
     for i,j in enumerate(filename,1):
         print(str(i).ljust(3),"|",j)
     print("--------------options--------------")
-    for i,j in enumerate(options,1):
+    for i,j in options.items():
         print(str(i).ljust(3),"|",j.center(15))
-    input()
+    print("-----------------------------------")
+
+    str1 = input()
+    if str1 == "1":
+        while True:
+            str1 = input("追加したいファイル名を入力してください(csv形式)")
+            find = re.search(r".+\.csv",str1)
+            if find:
+                filename.append(str1)
+                break
+            else:
+                print("ファイル名に誤りがあります")
