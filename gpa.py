cd2319aec810ca80267a6a1e9263425050751683
wadaka0821 <wadaka0821@gmail.com>
2019-10-12 18:02:49 +0900

    make code after main_options

--- gpa.py
+++ gpa.py
@@ -8,6 +8,14 @@ def check_filename(name):
         return False
 
 filename = list()
+options = ["ファイルの追加","ファイルの削除","ファイル操作","終了"]
 while True:
-    print("---------working space-----------")
-    
+    print("-----------working space-----------")
+    if filename == list():
+        print("Nothing")
+    for i,j in enumerate(filename,1):
+        print(str(i).ljust(3),"|",j)
+    print("--------------options--------------")
+    for i,j in enumerate(options,1):
+        print(str(i).ljust(3),"|",j.center(15))
+    input()
