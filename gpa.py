8196bd962d125b73a4983f2309d474748012a304
wadaka0821 <wadaka0821@gmail.com>
2019-10-12 18:35:19 +0900

    finished main_menu

--- gpa.py
+++ gpa.py
@@ -1,4 +1,5 @@
 import re
+import  score.Score as Score
 
 filename = list()
 options = {1:"ファイルの追加",2:"ファイルの削除",3:"ファイル操作",4:"終了"}
@@ -16,10 +17,28 @@ while True:
     str1 = input()
     if str1 == "1":
         while True:
-            str1 = input("追加したいファイル名を入力してください(csv形式)")
+            str1 = input("追加したいファイル名を入力してください(csv形式)\n")
             find = re.search(r".+\.csv",str1)
             if find:
-                filename.append(str1)
+                filename.append([str1,Score(str1)])
                 break
             else:
                 print("ファイル名に誤りがあります")
+    elif str1 == "2":
+        str1 = input("削除したいファイルのインデックスを入力してください(複数の場合は半角スペース区切り)\n")
+        str1 = sorted(str1.split(),reverse=True)
+        for i in str1:
+            try:
+                del filename[int(i)-1]
+            except:
+                print("入力されたインデックスに誤りがあります")
+    elif str1 == "3":
+        str1 = input("操作したいファイルのインデックスを入力してください\n")
+        try:
+            filename[int(str1)-1][1].menu()
+        except:
+            print("入力されたインデックスに誤りがあります")
+    elif str1 == "4":
+        break
+    else:
+        print("入力されたオプション番号に誤りがあります")
